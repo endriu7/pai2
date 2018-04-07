5c5d12e348a881a9b2a29e52165d74569c0ee945
Student <student@pc143p.uck.pk.edu.pl>
2018-04-07 14:57:08 +0200

    PAI

---
+++ View/lab1.php
@@ -0,0 +1,14 @@
+<?
+classStudent {var$surname;var$name;
+    function__construct($surname, $name){
+$this->surname = $nazwisko;
+$this->name = $name;
+}
+function
+setSurname($surname){
+$this->surname= $surname;}
+functiongetName(){
+return
+$this->name;}
+}
+?>
